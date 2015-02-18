760622dbd34359a6dd2f6fcf2d5ec6c02b89d92e
David Plews <david.plews@digital.justice.gov.uk>
2015-02-18 11:08:13 +0000

    Casefile Ctrl save fn
    WIP to save the data

--- app/scripts/controllers/casefile.js
+++ app/scripts/controllers/casefile.js
@@ -9,9 +9,11 @@
  */
 angular.module('digitalCasefileApp')
   .controller('CasefileCtrl', function ($scope, $routeParams, localstore) {
-    //console.log('CasefileCtrl', [$scope, $routeParams.urn, localstore.ref]);
+    console.log('CasefileCtrl');
 
     $scope.casefile = localstore.ref.get($routeParams.urn);
 
-
+    $scope.saveCtrl = function(data){
+      console.log('saveSection', data);
+    };
   });
