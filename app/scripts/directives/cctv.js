a18772141a4e53ce06e165d72499d09f8c37e4f4
David Plews <david.plews@digital.justice.gov.uk>
2015-02-18 11:03:50 +0000

    CCTV Changes
    - implemented jq-element-revealer
    - added store fn in isolate scope

--- app/scripts/directives/cctv.js
+++ app/scripts/directives/cctv.js
@@ -12,10 +12,11 @@ angular.module('digitalCasefileApp')
       templateUrl: 'scripts/directives/cctv.html',
       restrict: 'E',
       scope: {
-        cctv: '=data'
+        cctv: '=data',
+        store: '&'
       },
       replace: true,
-      link: function postLink() {
+      link: function postLink(scope) {
         //console.log('cctv link', [$scope, $element, $attrs]);
         angular.element.jqReveal();
       }
