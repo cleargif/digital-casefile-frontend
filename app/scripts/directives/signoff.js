87b083beab890a55b8c4a5c11a874486dcff11df
David Plews <david.plews@digital.justice.gov.uk>
2015-02-25 13:26:49 +0000

    Signoff: directive, spec, added to casefile

---
+++ app/scripts/directives/signoff.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name digitalCasefileApp.directive:signoff
+ * @description
+ * # signoff
+ */
+angular.module('digitalCasefileApp')
+  .directive('signoff', function () {
+    return {
+      templateUrl: 'scripts/directives/signoff.html',
+      restrict: 'E',
+      scope: {
+        signoff: '=data',
+        urn: '=urn',
+        store: '&'
+      },
+      replace: true,
+      link: function postLink() {
+        //console.log('signoff link', [$scope, $element, $attrs]);
+
+      }
+    };
+  });
