f406df10919aa36a8744464b532cfe52fb1b5292
David Plews <david.plews@digital.justice.gov.uk>
2015-02-26 14:34:08 +0000

    Offence: directive, spec, added to casefile

---
+++ app/scripts/directives/offence.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name digitalCasefileApp.directive:offence
+ * @description
+ * # offence
+ */
+angular.module('digitalCasefileApp')
+  .directive('offence', function () {
+    return {
+      templateUrl: 'scripts/directives/offence.html',
+      restrict: 'E',
+      scope: {
+        offence: '=data',
+        store: '&'
+      },
+      replace: true,
+      link: function postLink(scope) {
+        //console.log('offence link', [$scope, $element, $attrs]);
+
+        scope.offenceTitles = [{
+          value: '1',
+          name: 'Retail Theft'
+        }];
+
+        scope.pleaOptions = [{
+          value: '1',
+          name: 'Guilty'
+        }, {
+          value: '2',
+          name: 'Not Guilty'
+        }];
+      }
+    };
+  });
