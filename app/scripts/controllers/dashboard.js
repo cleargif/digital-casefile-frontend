f95c2bc656b7916173c8784678e7c4ae60bc6e65
David Plews <david.plews@digital.justice.gov.uk>
2015-03-02 11:49:24 +0000

    optimised getData for ngTable

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -10,6 +10,12 @@
 angular.module('digitalCasefileApp')
   .controller('DashboardCtrl', function ($scope, localstore, NgTableParams, $filter, $location) {
 
+    var defGetAll = localstore.getAll();
+
+    defGetAll.then(function (obj) {
+      $scope.data = obj.data;
+    });
+
     $scope.tableParams = new NgTableParams({
       page: 1, // show first page
       count: 10, // count per page
@@ -22,12 +28,13 @@ angular.module('digitalCasefileApp')
     }, {
       total: 0, // length of data
       getData: function ($defer, params) {
-        localstore.getAll().then(function () {
-          $scope.data = localstore.ref();
+
+        defGetAll.then(function () {
           // use build-in angular filter
           var filteredData = params.filter() ?
             $filter('filter')($scope.data, params.filter()) :
             $scope.data;
+
           var orderedData = params.sorting() ?
             $filter('orderBy')(filteredData, params.orderBy()) :
             $scope.data;
@@ -36,7 +43,6 @@ angular.module('digitalCasefileApp')
           $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 
         });
-
       }
     });
 
